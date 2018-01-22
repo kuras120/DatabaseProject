3c1b55ebafc5561c95fc287a5451650a9d0e9bcd
Wojciech <wojciech.kur96@gmail.com>
2018-01-22 23:25:04 +0100

    HomeController edited

--- FTPClient/FTPClient/Controllers/HomeController.cs
+++ FTPClient/FTPClient/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FTPClient.DAL;
+using FTPClient.Models;
 
 namespace FTPClient.Controllers
 {
     public class HomeController : Controller
     {
+
+        DataModel context = new DataModel();
+
         public ActionResult Index()
         {
             return View();
@@ -29,12 +34,38 @@ namespace FTPClient.Controllers
 
         public ActionResult Login()
         {
+
             return View("Index");
         }
 
         public ActionResult Signup()
         {
+
             return View("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Signup([Bind(Include = "loginInput,passwordInput")] User user)
+        {
+            var findUser = context.Users.Where(x => x.Login == user.Login).FirstOrDefault();
+            if (findUser == null)
+            {
+                if (ModelState.IsValid)
+                {
+                    context.Users.Add(user);
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            else
+            {
+                ViewBag.SignupError = true;
+                return View(ViewBag);
+            }
+            ViewBag.SignupError = false;
+            return View(ViewBag);
         }
     }
 }
